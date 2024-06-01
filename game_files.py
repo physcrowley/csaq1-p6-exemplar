0015e4435368a619a684210c23536eaffcfdfd04
physcrowley <53307335+physcrowley@users.noreply.github.com>
2024-05-31 22:04:24 -0400

    updated code

--- game_files.py
+++ game_files.py
@@ -1,19 +1,75 @@
+"""
+This module handles all file operations related to the game. This includes
+loading the game data from a file and loading or saving user game data.
 
-""" Extract and organize the game data """
+Exports these functions :
+- load_game() -> tuple[list, list]: Returns a ragged list of rooms and a flat list of room names.
+- show_saved_sessions(): Displays the list of saved user sessions.
+- save_session(user: str, session_name: str, items: list[str], location: str) -> bool: Saves the user game data to a file.
+- load_session(user: str, session_name: str) -> tuple[str, list[str]: Loads the user game data from a file.
 
+Exports these constants:
+- NAME: The index of the room name in the room data structure.
+- DESCRIPTION: The index of the room description in the room data structure.
+- OPTIONS: The index of the room options in the room data structure.
+- ITEMS: The index of the room items in the room data structure.
+- ITEM_MSGS: The index of the room item messages in the room data structure.
+- USER: The index of the user name in the user log structure.
+- USER_SESSION: The index of the user session name in the user log structure.
+- TIMESTAMP: The index of the timestamp in the user log structure.
+
+Exports these variables:
+- users: A set of user names.
+- user_log: A list of user log records.
+
+These functions and variables are reserved for internal use:
+- _add_user_file_to_log(user: str, session_name: str) -> None: Updates the log file and the in-memory collections.
+- _load_user_log(): Loads the user log file into memory.
+- _SAVE_DIR: The directory where user game files are saved.
+- _USER_FILE_LOG: The path to the user log file.
+- _GAME_FILE: The path to the main game file.
+"""
+
+import datetime
+import os
+
+# User game files
+_SAVE_DIR = "./data/saved_games/"
+_USER_FILE_LOG = os.path.join(_SAVE_DIR, "log.txt")
+
+# Main game file
+_GAME_FILE = "./data/game_data.txt"
+
+# Game file sections
 NAME = 0
 DESCRIPTION = 1
 OPTIONS = 2
 ITEMS = 3
 ITEM_MSGS = 4
 
-def load_game(file_path : str) -> tuple[list, list] :
+# Log file tuple elements
+USER = 0
+USER_SESSION = 1
+TIMESTAMP = 2
+
+users = set() # this variable is a candidate for removal but I haven't tested the edge cases
+user_log = []
+
+
+def load_game() -> tuple[list, list]:
+    """
+    This functions extracts the structured game data from the game file and returns it as
+    a ragged list of rooms and a flat list of room names.
+
+    The room list elements may be a single value or a list of values. Their contents
+    are described by the constants NAME, DESCRIPTION, OPTIONS, ITEMS, and ITEM_MSGS.
+    """
     # read data into memory
-    with open(file_path, "r") as file:
+    with open(_GAME_FILE, "r") as file:
         data = file.read()
-    
+
     # split the data into rooms
-    rooms = data.strip().split("###")  
+    rooms = data.strip().split("###")
 
     # split each room into sections
     for i in range(len(rooms)):
@@ -28,8 +84,77 @@ def load_game(file_path : str) -> tuple[list, list] :
         r[ITEMS] = r[ITEMS].strip().split(", ")
         # split item_message section into a list of item_messages
         r[ITEM_MSGS] = r[ITEM_MSGS].strip().split(", ")
-    
-    return rooms, room_names
-    
-rooms, room_names = load_game("./data/game_data.txt")
 
+    return rooms, room_names
+
+
+def _add_user_file_to_log(user: str, session_name: str) -> None:
+    """update the log file and the in-memory collections"""
+
+    now = str(datetime.datetime.now())
+    user = user.lower()
+
+    with open(_USER_FILE_LOG, "a") as file:
+        file.write(f"{user};{session_name};{now}\n")
+
+    user_log.append((user, session_name, now))
+
+    users.add(user)
+
+
+def _load_user_log() -> None:
+    """Pulls data from the user log file into memory"""
+    if len(user_log) > 0:  # already loaded
+        return
+
+    if not os.path.exists(_USER_FILE_LOG):  # no log file yet
+        return
+
+    with open(_USER_FILE_LOG, "r") as file:
+        # build both user collections
+        for line in file:
+            user_log.append((line.strip().split(";")))
+            users.add(user_log[-1][USER])
+
+
+def show_saved_sessions() -> None:
+    """Displays the list of saved user sessions"""
+    if len(user_log) == 0:  # not loaded yet
+        _load_user_log()
+
+    for i, record in enumerate(user_log):
+        user, session_name, timestamp = record
+        print(f"{i+1} : {session_name} - by {user} at {timestamp}")
+
+
+def save_session(user: str, session_name: str, items: list[str]) -> None:
+    """Saves the user session data to a file"""
+
+    # create the user file path
+    user_file = os.path.join(_SAVE_DIR, f"{user.lower()}_{session_name}.txt")
+
+    # write the user data to the file, overwriting any existing data
+    with open(user_file, "w") as file:
+        file.write(f"{','.join(items)}\n")
+
+    # update the log file
+    _add_user_file_to_log(user, session_name)
+
+
+def load_session(user: str, session_name: str) -> tuple[str, list[str]]:
+    """Loads the user session data from a file"""
+
+    if len(user_log) == 0:  # not loaded yet
+        _load_user_log()
+
+    if user.lower() not in users:
+        return None, None
+
+    # create the user file path
+    user_file = os.path.join(_SAVE_DIR, f"{user.lower()}_{session_name}.txt")
+
+    # read the user data from the file
+    with open(user_file, "r") as file:
+        items = file.readline().strip().split(",")
+
+    return items
--- main.py
+++ main.py
@@ -1,94 +1,176 @@
-# File: game.py
+"""
+Name: Exemplar by David Crowley
+Date: 2024-05-31
+File: main.py
+Description:
+This is a text-based adventure game where the player moves around a map
+and collects items. It uses file input and output for the game data and
+for saving and loading game sessions.
+
+The file IO logic is in the game_files module.
+
+This game also applies decomposition by breaking down the game logic into
+smaller, more coherent functions.
+
+The game script is at the bottom of the file.
+
+KNOWN ISSUES:
+- The game could probably use a univeral exit command. It wasn't in the plan, but it would be a nice feature.
 
 """
-Added items and an associated item-message list to each room
 
-Updated the file reading and game logic accordingly
-"""
+# Import data manipulation logic and data from game_files module
+from game_files import load_game
+from game_files import NAME, ITEMS, ITEM_MSGS
+from game_files import load_session, save_session, show_saved_sessions
+from game_files import user_log
 
-""" Extract and organize the game data """
+rooms, room_names = load_game()
 
-from game_files import NAME, ITEMS, ITEM_MSGS, rooms, room_names
-
-
-""" Game logic """
-
-# welcome message
-print("Welcome to the game!")
-print("You are about to be teleported into the game world.")
-print("Here are your options:")
-print("  [exit] or [enter]\n")
-choice = ""
-while choice not in ["exit", "enter"]:
-    choice = input("> ").lower()
-
-if choice == "exit":
-    print("Goodbye!")
-    exit()
-
-# game loop
 room = rooms[0]
 current_move = room_names[0]
 collected_items = []
-current_item = None
 
 
-while room[NAME] != "Computer" or current_move == "power button":
-    # the current exit condition is in the Computer room... but the power
-    # button item shows a special message before triggering the exit
+def fresh_game() -> tuple[str, list[str], list]:
+    """Returns starting current_move, collected_items, and room values."""
+    return room_names[0], [], rooms[0]
 
-    # interpret the current move as navigation or item selection
-    if current_move in room_names:
-        room_id = room_names.index(current_move)
-        room = rooms[room_id]
-        show_description = True
-    else:
-        item_id = room[ITEMS].index(current_move)
-        item_name = room[ITEMS][item_id]
-        item_msg = room[ITEM_MSGS][item_id]
-        # show item message
-        print("\n| ", item_msg)
-        # special exit condition
-        if item_name == "power button":
-            print("Shutting down... ", end="")
-            break
-        # update collection and show results
-        collected_items.append(item_name)
-        print("|  You have added", item_name, "to your collection.")
-        print("|  Inventory: ", collected_items)
-        # skip the room description
-        show_description = False
 
-    # extract room data
-    name, description, options, items, item_msgs = room
+def get_valid_option(options: list[str]) -> str:
+    """
+    Utility function that always returns a valid item from a list of options
+    given a user input matching at least the first 3 characters of an option.
 
-    # show room information
-    print("\n" + name)
-    if show_description:
-        print(description)
-    # show navigation options
-    print("\nYou can go here: ", end="")
-    for o in options:
-        print(f"  [{o}]  ", end="")
-    # show items available to pick up
-    print("\nOr you can pick up one of these items : ", end="")
-    available_items = [i for i in items if i not in collected_items]
-    for i in available_items:
-        print(f"  [{i}]  ", end="")
-    if len(available_items) == 0:
-        print("(nothing here)", end="")
-    print("\n")
-    # get the next valid move
-    print("Enter your move (at least 3 characters)")
-    valid_move = False
-    while not valid_move:
-        current_move = input("> ")
-        if len(current_move) < 3:
+    If the options have less than three characters, the length of the shortest
+    option is used as the minimum length for the user input.
+    """
+    least_chars = min(len(o) for o in options)
+    while True:
+        choice = input("> ")
+        if len(choice) < min(3, least_chars):
             continue
-        for e in options + available_items:
-            if e.lower().startswith(current_move.lower()):
-                current_move = e
-                valid_move = True
-                break
+        for o in options:
+            if o.lower().startswith(choice[:3].lower()):
+                return o
 
-print("Goodbye!")
+
+def end() -> None:
+    print("You have ended the game. Would you like to save your session?")
+    print("  [Yes]  [No]")
+    save = get_valid_option(["Yes", "No"])
+
+    if save == "No":
+        return
+
+    print("Enter your name:")
+    user_name = input("> ")
+    print("Enter a name for this session:")
+    session_name = input("> ")
+    save_session(user_name, session_name, collected_items)
+
+
+def welcome() -> None:
+    """
+    Get's the users choice for a game and returns an empty set of users if they
+    start a new game, the saved set of users if they load a game or None if they exit.
+    """
+    global collected_items
+
+    options = ["Start a new game", "Load a saved game", "Exit"]
+    START, LOAD, _ = options
+
+    print("Welcome to the game!")
+    print("You are about to be teleported into the game world.\n")
+    print("Here are your options. Type at least 3 characters to select an option.")
+    for o in options:
+        print(f"  [{o}]", end="")
+    print()
+    choice = get_valid_option(options)
+
+    if choice == START:
+        print("Let's start a new game!")
+
+    elif choice == LOAD:
+        print("Here are the saved sessions:")
+        show_saved_sessions()
+        print()
+        print("Enter the number of the session you wish to load.")
+        id = get_valid_option([str(i) for i in range(1, len(user_log) + 1)])
+        user, session, _ = user_log[int(id) - 1]
+        collected_items = load_session(user, session)
+        print(user, ", you are now equipped with the following items:")
+        print(collected_items)
+
+
+    else:  # choice == EXIT
+        exit()
+
+
+def play() -> None:
+    """Main game loop"""
+
+    global current_move, collected_items, room
+
+    while room[NAME] != "Computer" or current_move == "power button":
+        # the current exit condition is in the Computer room... but the power
+        # button item shows a special message before triggering the exit
+
+        # interpret the current move as navigation or item selection
+        if current_move in room_names:
+            room_id = room_names.index(current_move)
+            room = rooms[room_id]
+            show_description = True
+        else:
+            item_id = room[ITEMS].index(current_move)
+            item_name = room[ITEMS][item_id]
+            item_msg = room[ITEM_MSGS][item_id]
+            # show item message
+            print("\n| ", item_msg)
+            # special exit condition
+            if item_name == "power button":
+                print("Shutting down... ", end="")
+                break
+            # update collection and show results
+            collected_items.append(item_name)
+            print("|  You have added", item_name, "to your collection.")
+            print("|  Inventory: ", collected_items)
+            # skip the room description
+            show_description = False
+
+        # extract room data
+        name, description, options, items, _ = room
+
+        # show room information
+        print("\n\n" + name)
+        if show_description:
+            print(description)
+
+        # show navigation options
+        print("\nYou can go here: ", end="")
+        for o in options:
+            print(f"  [{o}]  ", end="")
+
+        # show items available to pick up
+        print("\nOr you can pick up one of these items : ", end="")
+        available_items = [i for i in items if i not in collected_items]
+        for i in available_items:
+            print(f"  [{i}]  ", end="")
+        if len(available_items) == 0:
+            print("(nothing here)", end="")
+        print("\n")
+
+        # get the next valid move
+        print("Enter your move (at least 3 characters)")
+        current_move = get_valid_option(options + available_items)
+
+
+#
+# GAME SCRIPT
+#
+
+while True:
+    welcome()  # program exit condition is here
+    play()
+    end()
+    current_move, collected_items, room = fresh_game()
